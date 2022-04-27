c2dba811a3d0b0f421b67182d0a566b256217102
Jackzhaozhiyuan <339952521@qq.com>
2022-04-27 22:34:02 +0800

    赵志远_second commit -- 这里是提交的说明，比如加了什么功能

--- src/main/java/controller/LogInController.java
+++ src/main/java/controller/LogInController.java
@@ -12,7 +12,7 @@ import java.util.List;
 @Controller
 public class LogInController {
 
-    // 你的修改  先pull  再Commit  勾选自己刚刚改过的代码所在的文件名 比如LogInController 其余的不勾 ，点击Commit and Push  文件名应该从蓝色变成了白色
+    // 你的修改  先pull  再Commit  勾选自己刚刚改过的代码所在的文件名 比如LogInController 其余的不勾 ，点击Commit and Push   文件名应该从蓝色变成了白色
     @Autowired
     public List<Integer> getInfo(){
         List<Integer> example = new ArrayList<>();

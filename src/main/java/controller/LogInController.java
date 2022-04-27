8c1c6f1738a94d9833d255b99ce3df336eccc665
Jackzhaozhiyuan <339952521@qq.com>
2022-04-27 22:30:03 +0800

    赵志远_second commit -- 这里是提交的说明，比如加了什么功能

--- src/main/java/controller/LogInController.java
+++ src/main/java/controller/LogInController.java
@@ -1,10 +1,21 @@
 package controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @author kun_mi
  */
 @Controller
 public class LogInController {
+
+    // 你的修改  先pull  再Commit  勾选自己刚刚改过的代码所在的文件名 比如LogInController 其余的不勾 ，点击Commit and Push
+    @Autowired
+    public List<Integer> getInfo(){
+        List<Integer> example = new ArrayList<>();
+        return example;
+    }
 }

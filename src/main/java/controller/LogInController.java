801ddfbccae62f845098211ab97d149297d769da
Jackzhaozhiyuan <339952521@qq.com>
2022-04-27 22:36:47 +0800

    赵志远_second commit -- 这里是提交的说明，比如加了123

--- src/main/java/controller/LogInController.java
+++ src/main/java/controller/LogInController.java
@@ -13,6 +13,7 @@ import java.util.List;
 public class LogInController {
 
     // 你的修改  先pull  再Commit  勾选自己刚刚改过的代码所在的文件名 比如LogInController 其余的不勾 ，点击Commit and Push   文件名应该从蓝色变成了白色
+    // 123
     @Autowired
     public List<Integer> getInfo(){
         List<Integer> example = new ArrayList<>();
